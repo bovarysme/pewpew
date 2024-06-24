8ed35a932a723448be2ea4a7aee369332d6e974e
bovarysme <bovarysme@users.noreply.github.com>
2024-06-24 22:41:14 +0200

    Use the PCG RNG

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,16 @@
 #ifndef PEWPEW_UTILS_H_
 #define PEWPEW_UTILS_H_
 
-#include <cstdlib>
+#include <random>
 
+#include "../third_party/pcg-cpp/include/pcg_random.hpp"
 #include "float.h"
 
-inline Float RandomFloat() { return rand() / (RAND_MAX + 1.0); }
+inline Float RandomFloat() {
+  static pcg_extras::seed_seq_from<std::random_device> seed_source;
+  static pcg32 rng(seed_source);
+  return rng() / (rng.max() + 1.0);
+}
 
 inline Float RandomFloat(double min, double max) {
   return min + (max - min) * RandomFloat();
